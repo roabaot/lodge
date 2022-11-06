919f3b0784e04b9354eadf24a97c2f0fe2663d86
ahmed <a-ahmed987@outlook.com>
2022-11-06 06:52:25 +0300

    add pages

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <MainHero id="home" />
+    <MainBestPrice />
+    <MainAbout id="about" />
+    <MainJewellery id="jewellery" />
+    <MainSpecial />
+    <MainTest />
+    <MainContact />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
